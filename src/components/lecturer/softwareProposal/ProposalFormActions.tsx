146eecbd5f45cd185b45568e7a171b167bd9d277
Phan Hoang Tan <phanhoangtan.job@gmail.com>
2025-09-20 11:50:05 +0700

    refactor: remove cancel button from proposal form actions

--- src/components/lecturer/softwareProposal/ProposalFormActions.tsx
+++ src/components/lecturer/softwareProposal/ProposalFormActions.tsx
@@ -5,19 +5,13 @@ import { Send, X } from "lucide-react";
 interface ProposalFormActionsProps {
   isSubmitting: boolean;
   isEditMode: boolean;
-  onCancel: () => void;
 }
-
 const ProposalFormActions: React.FC<ProposalFormActionsProps> = ({
   isSubmitting,
   isEditMode,
-  onCancel,
 }) => {
   return (
     <div className="flex justify-end space-x-3 pt-4 border-t border-gray-200">
-      <Button size="large" onClick={onCancel} icon={<X className="w-4 h-4" />}>
-        Hủy
-      </Button>
       <Button
         type="primary"
         size="large"
